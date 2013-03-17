9092c2aa9113b7a4ce38fb0924066fcd84c3d3ff
Clément Bourgeois <moonpyk@gmail.com>
2013-03-17 22:18:09 +0100

    Work on the web frontend

---
+++ SQLServerBackupTool.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQLServerBackupTool.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string id)
+        {
+            return new EmptyResult();
+        }
+    }
+}
