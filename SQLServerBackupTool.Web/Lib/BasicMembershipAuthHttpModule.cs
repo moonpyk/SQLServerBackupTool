3a8b5c8ce963375780ee48ae5d19c1a3b2433a0b
Clément Bourgeois <moonpyk@gmail.com>
2013-03-18 21:04:13 +0100

    Basic auth membership module

---
+++ SQLServerBackupTool.Web/Lib/BasicMembershipAuthHttpModule.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Security;
+
+namespace SQLServerBackupTool.Web.Lib
+{
+    public class BasicMembershipAuthHttpModule : IHttpModule
+    {
+        private static string _realm = "Access restricted area";
+
+        public static string Realm
+        {
+            get { return _realm; }
+            set { _realm = value; }
+        }
+
+        public void Init(HttpApplication context)
+        {
+            // Register event handlers
+            context.AuthenticateRequest += OnApplicationAuthenticateRequest;
+            context.EndRequest += OnApplicationEndRequest;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private static void SetPrincipal(IPrincipal principal)
+        {
+            Thread.CurrentPrincipal = principal;
+
+            if (HttpContext.Current != null)
+            {
+                HttpContext.Current.User = principal;
+            }
+        }
+
+        private static bool AuthenticateUser(string credentials)
+        {
+            bool validated;
+
+            try
+            {
+                var encoding = Encoding.GetEncoding("iso-8859-1");
+                credentials = encoding.GetString(Convert.FromBase64String(credentials));
+
+                var separator = credentials.IndexOf(':');
+                var name = credentials.Substring(0, separator);
+                var password = credentials.Substring(separator + 1);
+
+                validated = Membership.ValidateUser(name, password);
+                if (validated)
+                {
+                    var user = Membership.GetUser(name, true);
+
+                    if (user != null)
+                    {
+                        var identity = new GenericIdentity(user.UserName);
+
+                        string[] rolesForUser = null;
+
+                        if (Roles.Enabled)
+                        {
+                            rolesForUser = Roles.GetRolesForUser(user.UserName);
+                        }
+
+                        SetPrincipal(new GenericPrincipal(identity, rolesForUser));
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                // Credentials were not formatted correctly.
+                validated = false;
+            }
+
+            return validated;
+        }
+
+        private static void OnApplicationAuthenticateRequest(object sender, EventArgs e)
+        {
+            var request = HttpContext.Current.Request;
+            var authHeader = request.Headers["Authorization"];
+            if (authHeader == null)
+            {
+                return;
+            }
+
+            var authHeaderVal = AuthenticationHeaderValue.Parse(authHeader);
+
+            // RFC 2617 sec 1.2, "scheme" name is case-insensitive
+            if (
+                authHeaderVal.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase) &&
+                authHeaderVal.Parameter != null
+                )
+            {
+                AuthenticateUser(authHeaderVal.Parameter);
+            }
+        }
+
+        // If the request was unauthorized, add the WWW-Authenticate header 
+        // to the response.
+        private static void OnApplicationEndRequest(object sender, EventArgs e)
+        {
+            var response = HttpContext.Current.Response;
+            if (response.StatusCode == 401)
+            {
+                response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", Realm));
+            }
+        }
+    }
+}
