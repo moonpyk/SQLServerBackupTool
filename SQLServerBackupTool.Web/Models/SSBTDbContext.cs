4c6a59fa7ae1bcc141637462dc7db786ca4a2f8e
Clément Bourgeois <moonpyk@gmail.com>
2013-03-20 00:40:47 +0100

    Bit of fight with EF5, not many changes, but a lot of things learned.

---
+++ SQLServerBackupTool.Web/Models/SSBTDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+
+namespace SQLServerBackupTool.Web.Models
+{
+    public class SSBTDbContext : DbContext
+    {
+        // Vous pouvez ajouter du code personnalisé à ce fichier. Les modifications ne seront pas remplacées.
+        // 
+        // Si vous voulez qu’Entity Framework abandonne et régénère la base de données
+        // automatiquement à chaque fois que vous modifiez le schéma du modèle, ajoutez le code
+        // suivant à la méthode Application_Start dans le fichier Global.asax.
+        // Remarque : cette opération supprime et recrée la base de données à chaque modification du modèle.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<SQLServerBackupTool.Web.Models.SSBTDbContext>());
+
+        public SSBTDbContext()
+            : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<UserDatabase> UserDatabases { get; set; }
+    }
+}
---
+++ SQLServerBackupTool.Web/Models/UserDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SQLServerBackupTool.Web.Models
+{
+    public partial class UserDatabase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50), Required]
+        public string Username { get; set; }
+
+        [MaxLength(50), Required]
+        public string DatabaseName { get; set; }
+    }
+}
