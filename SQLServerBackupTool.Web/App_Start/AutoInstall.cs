d44eeac1dad7d63e7ffb7d3b2f14f03153de8570
Clément Bourgeois <moonpyk@gmail.com>
2013-03-23 21:11:13 +0100

    Autoinstall proof of concept

---
+++ SQLServerBackupTool.Web/App_Start/AutoInstall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Security;
+using Dapper;
+using SQLServerBackupTool.Web;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(AutoInstall), "PreStart")]
+[assembly: WebActivator.PostApplicationStartMethod(typeof(AutoInstall), "PostStart")]
+
+namespace SQLServerBackupTool.Web
+{
+
+    public static class AutoInstall
+    {
+        public static void PreStart()
+        {
+            Debug.WriteLine("PreStart");
+        }
+
+        public static void PostStart()
+        {
+            MembershipUser defaultUser = null;
+            if (Membership.GetAllUsers().Count == 0)
+            {
+                defaultUser = Membership.CreateUser("admin", "password");
+            }
+
+            if (!Roles.RoleExists("Admin"))
+            {
+                Roles.CreateRole("Admin");
+
+                if (defaultUser != null)
+                {
+                    Roles.AddUserToRole(defaultUser.UserName, "Admin");
+                }
+            }
+        }
+    }
+}
