79e04d9d0d10ae018ebdf947e2ed2d7104c46d25
Clément Bourgeois <moonpyk@gmail.com>
2013-03-31 22:44:51 +0200

    Interface I extracted for reference

---
+++ SQLServerBackupTool.Web/Lib/Interfaces/IMembershipUser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace SQLServerBackupTool.Web.Lib.Interfaces
+{
+    public interface IMembershipUser
+    {
+        string Email
+        {
+            get;
+            set;
+        }
+
+        string Comment
+        {
+            get;
+            set;
+        }
+
+        bool IsApproved
+        {
+            get;
+            set;
+        }
+
+        DateTime LastLoginDate
+        {
+            get;
+            set;
+        }
+
+        DateTime LastActivityDate
+        {
+            get;
+            set;
+        }
+
+        /*
+         * Readonly
+         */
+
+        string UserName
+        {
+            get;
+        }
+
+        object ProviderUserKey
+        {
+            get;
+        }
+
+        bool IsLockedOut
+        {
+            get;
+        }
+
+        DateTime LastLockoutDate
+        {
+            get;
+        }
+
+        DateTime CreationDate
+        {
+            get;
+        }
+
+        DateTime LastPasswordChangedDate
+        {
+            get;
+        }
+
+        bool IsOnline
+        {
+            get;
+        }
+
+        string PasswordQuestion
+        {
+            get;
+        }
+
+        string ProviderName
+        {
+            get;
+        }
+
+        void Update();
+
+        string GetPassword();
+        string GetPassword(string passwordAnswer);
+        string GetPassword(bool throwOnError);
+        string GetPassword(string answer, bool throwOnError);
+
+        bool ChangePassword(string oldPassword, string newPassword);
+        bool ChangePassword(string oldPassword, string newPassword, bool throwOnError);
+        bool ChangePasswordQuestionAndAnswer(string password, string newPasswordQuestion, string newPasswordAnswer);
+
+        string ResetPassword();
+        string ResetPassword(string passwordAnswer);
+        string ResetPassword(bool throwOnError);
+        string ResetPassword(string passwordAnswer, bool throwOnError);
+
+        bool UnlockUser();
+    }
+}
