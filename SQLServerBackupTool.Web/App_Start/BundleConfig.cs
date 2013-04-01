4fa4d1c3f00ad5dca8155c48b4132958d2fc3b40
Clément Bourgeois <moonpyk@gmail.com>
2013-04-01 17:20:23 +0200

    Added jquery.validate.unobtrusive.js

--- SQLServerBackupTool.Web/App_Start/BundleConfig.cs
+++ SQLServerBackupTool.Web/App_Start/BundleConfig.cs
@@ -14,6 +14,11 @@ namespace SQLServerBackupTool.Web
                 "~/Scripts/jquery-{version}.js"
             ));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive.js"
+            ));
+
             bundles.Add(new ScriptBundle("~/bundles/ssbtw").Include(
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/jquery.pldr.js",
