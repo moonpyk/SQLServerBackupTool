52f7bb734b1823f647940ced70aa10f6f2994f3a
Clément Bourgeois <moonpyk@gmail.com>
2013-04-13 22:41:53 +0200

    MSBuild version targets

--- SQLServerBackupTool.Web/Properties/AssemblyInfo.cs
+++ SQLServerBackupTool.Web/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("SQLServerBackupTool.Web")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SQLServerBackupTool.Web")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("SQLServerBackupToolkit.Web")]
+// [assembly: AssemblyDescription("")]
+// [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("moonpyk.net")]
+[assembly: AssemblyProduct("SQLServerBackupToolkit")]
+[assembly: AssemblyCopyright("Copyright © Moonpyk 2013")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
@@ -22,14 +22,3 @@ using System.Runtime.InteropServices;
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("64578783-a9e3-43a0-a73f-4f362327ee13")]
 
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
